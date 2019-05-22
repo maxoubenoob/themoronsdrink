f96bbea9f5f84329d6c56d22f75d057f9e69069a
maxoubenoob <maxime.brunet@live.fr>
2019-05-22 16:39:55 +0200

    Change the database config

--- config/database.php
+++ config/database.php
@@ -44,11 +44,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'http://maximebrunet.alwaysdata.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'maximebrunet_themoronsdrink'),
+            'username' => env('DB_USERNAME', '184220'),
+            'password' => env('DB_PASSWORD', '7365alwaysdata'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
